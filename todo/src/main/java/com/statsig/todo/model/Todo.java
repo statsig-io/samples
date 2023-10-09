a42f435e88f001e050161adf15c718ccf522f2db
upendras-ltts <upendra.singh@ltts.com>
2023-10-09 09:31:26 +0530

    minor change

--- todo/src/main/java/com/statsig/todo/model/Todo.java
+++ todo/src/main/java/com/statsig/todo/model/Todo.java
@@ -32,10 +32,10 @@ public class Todo {
 
     private boolean lastViewed;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime createdDate;
 
-    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern="yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
     private LocalDateTime modifiedDate;
 }
 
