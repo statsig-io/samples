693f9c24d34920ad40be08e570873384d04646d1
lttsNilesh <nilesh.walavalkar@ltts.com>
2023-11-16 16:32:03 +0530

    Initial repo commit

---
+++ Flutter_mobile/sample_todo_app/lib/screens/tasks/tasks_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:sample_todo_app/routing/go_router.dart';
+
+import '../../widgets/todo_list_view.dart';
+
+class TasksScreen extends StatefulWidget {
+  const TasksScreen({super.key});
+
+  @override
+  State<TasksScreen> createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Tasks'),
+      ),
+      body: const TodoListView(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.pushNamed(AppRoute.addTodoScreen.name);
+        },
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
