a4fb98f655dbfa02352cf1ba966c11baed7832f6
lttsNilesh <nilesh.walavalkar@ltts.com>
2023-11-30 18:03:16 +0530

    app lifecycle event in homescreen after login success.

--- Flutter_mobile/sample_todo_app/lib/main.dart
+++ Flutter_mobile/sample_todo_app/lib/main.dart
@@ -17,56 +17,6 @@ class MainApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return const MaterialApp(
-      home: DetectLifecycle(),
-    );
-  }
-}
-
-class DetectLifecycle extends ConsumerStatefulWidget {
-  const DetectLifecycle({Key? key}) : super(key: key);
-
-  @override
-  ConsumerState<ConsumerStatefulWidget> createState() {
-    return _DetectLifecycleState();
-  }
-}
-
-class _DetectLifecycleState extends ConsumerState<DetectLifecycle>
-    with WidgetsBindingObserver {
-  final appOpened = "CLIENT_TODO_APP_OPENED";
-  final appBackgrounded = "CLIENT_TODO_APP_BACKGROUND";
-
-  @override
-  void initState() {
-    WidgetsBinding.instance.addObserver(this);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    switch (state) {
-      case AppLifecycleState.resumed:
-        Statsig.logEvent(appOpened);
-        break;
-      case AppLifecycleState.inactive:
-        Statsig.logEvent(appBackgrounded);
-        break;
-      case AppLifecycleState.paused:
-      case AppLifecycleState.detached:
-      case AppLifecycleState.hidden:
-        break;
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
     final goRouter = ref.watch(goRouterProvider);
     return DynamicColorBuilder(
       builder: (lightColorScheme, darkColorScheme) => MaterialApp.router(
